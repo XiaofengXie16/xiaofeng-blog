673f688c0d699ecd3d0e3e320fa70b0bcbfdbb31
Xiaofeng Xie <xiaofeng02111@gmail.com>
2024-11-04 21:53:42 -0500

    Added error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -4,17 +4,62 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 import { withSentry } from "@sentry/remix";
 
 import "./tailwind.css";
 
 export const meta = () => {
-    return [
-        { title: "Xiaofeng's Blog" },
-        { charset: "utf-8" },
-        { viewport: "width=device-width,initial-scale=1" },
-    ];
+  return [
+    { title: "Xiaofeng's Blog" },
+    { charset: "utf-8" },
+    { viewport: "width=device-width,initial-scale=1" },
+  ];
+};
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <html
+      lang="en"
+      className="h-full bg-gray-700 text-gray-300"
+    >
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="flex justify-center items-center min-h-screen">
+        <div className="relative p-8 max-w-xl mx-4 text-center">
+          <h1 className="text-3xl font-bold text-pink-300 mb-6 max-w-full">
+            Oops! Something went wrong...
+          </h1>
+          <p className="text-lg text-gray-400 mb-6">
+            {error instanceof Error
+              ? error.message
+              : "An unexpected error occurred. Don’t worry, it happens!"}
+          </p>
+
+          {error instanceof Error && (
+            <pre className="bg-gray-700 p-4 rounded-lg text-sm text-gray-400 overflow-x-auto mb-6 max-h-32 border border-gray-600 animate-fade-in">
+              {error.stack}
+            </pre>
+          )}
+
+          <a
+            href="/"
+            className="inline-block bg-gradient-to-br from-pink-500 to-pink-600 text-white text-sm font-semibold py-3 px-8 rounded-full shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl animate-fade-in"
+          >
+            Take me Home
+          </a>
+        </div>
+        <Scripts />
+      </body>
+    </html>
+  );
 };
 function App() {
   return (
@@ -33,4 +78,5 @@ function App() {
     </html>
   );
 }
+
 export default withSentry(App);
