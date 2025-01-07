7c72f36bae4615f27810e87d2170af78612f6898
Xiaofeng Xie <xiaofeng02111@gmail.com>
2025-01-06 22:24:13 -0500

    Updated dependencies

--- app/components/Card/BlogCard.tsx
+++ app/components/Card/BlogCard.tsx
@@ -6,16 +6,16 @@ type CardParams = {
 
 export const BlogCard = ({  name, description, link }: CardParams) => {
   return (
-    <div className={"flex w-full flex-col gap-1 rounded-2xl hover:bg-gray-600"}>
+    <div className={"flex w-full flex-col gap-1 rounded-2xl"}>
       <a
         href={link}
         className={
-          "flex rounded gap-1 border-2 border-transparent hover:border-cyan-200 pr-6 pt-6 pb-6"
+          "flex rounded gap-1 pr-6 pt-6 pb-6"
         }
         rel="prefetch"
       >
         <div className={"sm:w-2/3 "}>
-          <h1 className={"text-2xl text-pink-200 pb-2.5"}>{name}</h1>
+          <h1 className={"text-2xl text-pink-200 pb-2.5 hover:underline"}>{name}</h1>
           <h2 className={"text-white break-words"}>{description}</h2>
         </div>
       </a>
