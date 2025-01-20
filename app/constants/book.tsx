793b9b12067794eb3dd5a429af141a5ab00acb81
Xiaofeng Xie <xiaofeng02111@gmail.com>
2025-01-20 17:36:54 -0500

    Added why nations fail to the book list

--- app/constants/book.tsx
+++ app/constants/book.tsx
@@ -3,8 +3,14 @@ import TheStaffEngineerPath from "~/assets/images/the_staff_engineer_path.jpg";
 import APhilosophyOfSoftwareDesign from "~/assets/images/a_philosophy_of_software_design.jpg";
 import SimpliySaid from "~/assets/images/simply_said.png";
 import DesigningDataIntensiveApplications from "~/assets/images/designing_data_intensive_applications.png";
+import WhyNationsFail from "~/assets/images/why_nations_fail.jpg";
 
 export const BOOKS = [
+  {
+    name: "Why Nations Fail: The Origins of Power, Prosperity, and Poverty",
+    icon: WhyNationsFail,
+    link: "https://ia801506.us.archive.org/27/items/WhyNationsFailTheOriginsODaronAcemoglu/Why-Nations-Fail_-The-Origins-o-Daron-Acemoglu.pdf",
+  },
   {
     name: "The Staff Engineer's Path",
     icon: TheStaffEngineerPath,
@@ -25,9 +31,9 @@ export const BOOKS = [
     icon: SimpliySaid,
     link: "https://www.exec-comm.com/product/simply-said/",
   },
-    {
-        name:"Designing Data-Intensive Applications",
-        icon: DesigningDataIntensiveApplications,
-        link:"https://dataintensive.net/"
-    }
+  {
+    name: "Designing Data-Intensive Applications",
+    icon: DesigningDataIntensiveApplications,
+    link: "https://dataintensive.net/"
+  }
 ];
