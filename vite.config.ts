4d174850b376a12ec4932154bb3c4ac9ed530f42
Xiaofeng Xie <xiaofeng02111@gmail.com>
2025-01-20 16:01:32 -0500

    Updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
-
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         reactRouter(),
         tsconfigPaths()
     ],
